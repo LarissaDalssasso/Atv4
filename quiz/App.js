776559624a68b02d54a8fe4d142e11034ad937c3
LarissaDalssasso <larissadalssasso10@gmail.com>
2024-11-07 10:12:41 -0300

    33

--- quiz/App.js
+++ quiz/App.js
@@ -1,11 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Button, Image, TouchableOpacity, ImageBackground,ScrollView} from 'react-native';
+import { Platform, Text, View, StyleSheet, Button, Image, TouchableOpacity, ImageBackground, ScrollView } from 'react-native';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
 const PilhasTelas = createNativeStackNavigator()
+
+function escolherPais(pais) {
+    if (pais == "Brasil") {
+        navigation.navigate("VisualizarQuizBrasil")
+    }else
+}
 
 function TelaInicial({ route, navigation }) {
     const [latitude, setLatitude] = useState(0.0);
@@ -48,7 +54,7 @@ function TelaInicial({ route, navigation }) {
             setAltitude(alt);
             setPais(await buscarPais(lat, long));
         };
-        
+
         buscarCoordenadas();
     }, []);
 
@@ -60,14 +66,220 @@ function TelaInicial({ route, navigation }) {
             <View style={styles.containerInicial}>
                 <Text style={styles.titleInicial}>Teste seus conhecimentos e descubra o quanto você sabe sobre o mundo!</Text>
                 <Text style={styles.textoInicial}>{pais ? pais : "Carregando..."}</Text>
-                <Button title="PERGUNTAS" color="#523C20" onPress={() => { navigation.navigate("VisualizarQuiz") }} />
+                <Button title="PERGUNTAS" color="#523C20" onPress={() => { escolherPais(pais) }} />
             </View>
         </ImageBackground>
     );
 }
 
-function VisualizarQuiz({ route, navigation }) {
-    const [respostasSelecionadas, setRespostasSelecionadas] = useState([null, null,null]);
+// perguntas do Brasil
+function VisualizarQuizBrasil({ route, navigation }) {
+    const [respostasSelecionadas, setRespostasSelecionadas] = useState([null, null, null]);
+
+    // Respostas corretas para cada pergunta
+    const respostasCorretas = [
+        '38 m', // Resposta correta para a primeira pergunta
+        'A arara-azul', // Resposta correta para a segunda pergunta
+        'Em tigelas'
+    ];
+
+    const respostaEscolhida = (resposta, index) => {
+        const novasRespostas = [...respostasSelecionadas];
+        if (novasRespostas[index] === null) { // Verifica se a resposta para essa pergunta ainda não foi selecionada
+            novasRespostas[index] = resposta;
+            setRespostasSelecionadas(novasRespostas);
+        }
+    };
+
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.title}>Brasil</Text>
+            {/* Primeira Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>Quantos metros têm a estátua mais famosa do Brasil?</Text>
+                <Image style={styles.image} source={require('./assets/images/cristo-redentor.jpg')} />
+                <View style={styles.alternativas}>
+                    {['35 m', '38 m', '50 m', '46 m'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[0] === resposta && { backgroundColor: resposta === respostasCorretas[0] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 0)}
+                            disabled={!!respostasSelecionadas[0]} // Desabilita o TouchableOpacity após uma seleção
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            {/* Segunda Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>Qual animal é considerado o símbolo nacional do Brasil?</Text>
+                <Image style={styles.image} source={require('./assets/images/animais.jpg')} />
+                <View style={styles.alternativas}>
+                    {['O lobo-guará', 'O tamanduá-bandeira', 'O jaguar', 'A arara-azul'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[1] === resposta && { backgroundColor: resposta === respostasCorretas[1] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 1)}
+                            disabled={!!respostasSelecionadas[1]} // Desabilita o TouchableOpacity após uma seleção
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            {/* Terceira Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>O açaí é uma fruta típica da região amazônica do Brasil, conhecida por seu sabor e benefícios nutricionais. Qual é a forma mais comum de consumo do açaí nas regiões do Norte do Brasil?</Text>
+                <Image style={styles.image} source={require('./assets/images/acai.jpg')} />
+                <View style={styles.alternativas}>
+                    {['Em sucos', 'Em sorvetes', 'Em tigelas', 'Em smoothies'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[2] === resposta && { backgroundColor: resposta === respostasCorretas[2] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 2)}
+                            disabled={!!respostasSelecionadas[2]} // Desabilita o TouchableOpacity após uma seleção
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            <Button
+                title="Finalizar QUIZ"
+                color="#523C20"
+                onPress={() => navigation.navigate("VisualizarResultado")}
+                disabled={!respostasSelecionadas[0] || !respostasSelecionadas[1]} // Desabilita o botão até que ambas as respostas sejam selecionadas
+            />
+        </ScrollView>
+    );
+}
+
+// Perguntas da Russia
+function VisualizarQuizRussia({ route, navigation }) {
+    const [respostasSelecionadas, setRespostasSelecionadas] = useState([null, null, null], null);
+
+    // Respostas corretas para cada pergunta
+    const respostasCorretas = [
+        'O Lago dos Cisnes',
+        'Moscou',
+        'Urso',
+        'Matryoshka'
+    ];
+
+    const respostaEscolhida = (resposta, index) => {
+        const novasRespostas = [...respostasSelecionadas];
+        if (novasRespostas[index] === null) { // Verifica se a resposta para essa pergunta ainda não foi selecionada
+            novasRespostas[index] = resposta;
+            setRespostasSelecionadas(novasRespostas);
+        }
+    };
+
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.title}>Russia</Text>
+            {/* Primeira Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>O balé é uma parte importante da cultura russa. Qual é o nome do balé clássico russo, composto por Pyotr Tchaikovsky, que conta a história de uma princesa transformada em um animal?</Text>
+                <Image style={styles.image} source={require('./assets/images/baleRusso.jpg')} />
+                <View style={styles.alternativas}>
+                    {['Quebra-Nozes', 'Giselle', 'O Lago dos Cisnes', 'A Bela Adormecida'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[0] === resposta && { backgroundColor: resposta === respostasCorretas[0] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 0)}
+                            disabled={!!respostasSelecionadas[0]} // Desabilita o TouchableOpacity após uma seleção
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            {/* Segunda Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>Qual é a capital da Rússia?</Text>
+                <Image style={styles.image} source={require('./assets/images/mapaRussia.jpg')} />
+                <View style={styles.alternativas}>
+                    {['São Petersburgo', 'Moscou', 'Kazan', 'Novosibirsk'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[1] === resposta && { backgroundColor: resposta === respostasCorretas[1] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 1)}
+                            disabled={!!respostasSelecionadas[1]} 
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            {/* Terceira Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>Qual animal é conhecido como o símbolo nacional da Rússia?</Text>
+                <Image style={styles.image} source={require('./assets/images/animalRussia.jpg')} />
+                <View style={styles.alternativas}>
+                    {['Águia', 'Lobo', 'Urso', 'Raposa'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[2] === resposta && { backgroundColor: resposta === respostasCorretas[2] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 2)}
+                            disabled={!!respostasSelecionadas[2]} 
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            {/* Quarta Pergunta */}
+            <View style={styles.cardContainer}>
+                <Text style={styles.pergunta}>As bonecas russas famosas por serem feitas de madeira e por se encaixam uma dentro da outra são conhecidas como?</Text>
+                <Image style={styles.image} source={require('./assets/images/animalRussia.jpg')} />
+                <View style={styles.alternativas}>
+                    {['Matryoshka', 'Babushka', 'Troika', 'Ushanka'].map((resposta, index) => (
+                        <TouchableOpacity
+                            key={resposta}
+                            style={[
+                                styles.cardContainer2,
+                                respostasSelecionadas[3] === resposta && { backgroundColor: resposta === respostasCorretas[3] ? 'green' : 'red' },
+                            ]}
+                            onPress={() => respostaEscolhida(resposta, 3)}
+                            disabled={!!respostasSelecionadas[3]}
+                        >
+                            <Text style={styles.alternativa}>{resposta}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            <Button
+                title="Finalizar QUIZ"
+                color="#523C20"
+                onPress={() => navigation.navigate("VisualizarResultado")}
+                disabled={!respostasSelecionadas[0] || !respostasSelecionadas[1]} // Desabilita o botão até que ambas as respostas sejam selecionadas
+            />
+        </ScrollView>
+    );
+}
+
+// Perguntas da China
+function VisualizarQuizChina({ route, navigation }) {
+    const [respostasSelecionadas, setRespostasSelecionadas] = useState([null, null, null]);
 
     // Respostas corretas para cada pergunta
     const respostasCorretas = [
@@ -169,10 +381,51 @@ function VisualizarResultado({ route, navigation }) {
             <Text style={styles.resultado}>
                 Resultado: <Text style={resultado.correta ? styles.correto : styles.incorreto}>{resultado.correta ? 'Correto!' : 'Incorreto!'}</Text>
             </Text>
-            <Button 
-                title="Voltar" 
-                color="#523C20" 
-                onPress={() => navigation.navigate("TelaInicial")} 
+            <Button
+                title="Voltar"
+                color="#523C20"
+                onPress={() => navigation.navigate("TelaInicial")}
+            />
+        </View>
+    );
+}
+function VisualizarResultado({ route, navigation }) {
+    const { resultado } = route.params; // Pegando os resultados passados
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Resultado do Quiz</Text>
+            <Text style={styles.resultado}>
+                Você selecionou: <Text style={resultado.correta ? styles.correto : styles.incorreto}>{resultado.respostaSelecionada}</Text>
+            </Text>
+            <Text style={styles.resultado}>
+                Resultado: <Text style={resultado.correta ? styles.correto : styles.incorreto}>{resultado.correta ? 'Correto!' : 'Incorreto!'}</Text>
+            </Text>
+            <Button
+                title="Voltar"
+                color="#523C20"
+                onPress={() => navigation.navigate("TelaInicial")}
+            />
+        </View>
+    );
+}
+
+function VisualizarResultado({ route, navigation }) {
+    const { resultado } = route.params; // Pegando os resultados passados
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Resultado do Quiz</Text>
+            <Text style={styles.resultado}>
+                Você selecionou: <Text style={resultado.correta ? styles.correto : styles.incorreto}>{resultado.respostaSelecionada}</Text>
+            </Text>
+            <Text style={styles.resultado}>
+                Resultado: <Text style={resultado.correta ? styles.correto : styles.incorreto}>{resultado.correta ? 'Correto!' : 'Incorreto!'}</Text>
+            </Text>
+            <Button
+                title="Voltar"
+                color="#523C20"
+                onPress={() => navigation.navigate("TelaInicial")}
             />
         </View>
     );
@@ -188,8 +441,18 @@ export default function App() {
                     options={{ title: "Home" }}
                 ></PilhasTelas.Screen>
                 <PilhasTelas.Screen
-                    name="VisualizarQuiz"
-                    component={VisualizarQuiz}
+                    name="VisualizarQuizBrasil"
+                    component={VisualizarQuizBrasil}
+                    options={{ title: "Quiz" }}
+                ></PilhasTelas.Screen>
+                <PilhasTelas.Screen
+                    name="VisualizarQuizRussia"
+                    component={VisualizarQuizRussia}
+                    options={{ title: "Quiz" }}
+                ></PilhasTelas.Screen>
+                <PilhasTelas.Screen
+                    name="VisualizarQuizChina"
+                    component={VisualizarQuizChina}
                     options={{ title: "Quiz" }}
                 ></PilhasTelas.Screen>
                 <PilhasTelas.Screen
@@ -212,7 +475,7 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 20,
-        textAlign:'center'
+        textAlign: 'center'
     },
     cardContainer: {
         marginBottom: 20,
@@ -220,7 +483,7 @@ const styles = StyleSheet.create({
     pergunta: {
         fontSize: 18,
         marginBottom: 10,
-        textAlign:'center'
+        textAlign: 'center'
     },
     image: {
         width: '100%',
